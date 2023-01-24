af4d764c9a3364cccbe087133ae72ab657321a0a
folarin <folarintaiye@gmail.com>
2023-01-24 20:46:00 +0000

    work on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Header.css"
-import {link} from "react-router-dom"
+
 
 export default function Header() {
   return (
     <div className='header'>
       <div className='header__details'>
-        <h3>Lady-K Make up collection</h3>
-        <h1>Makeup Enhances  <br></br> Your Natural Beauty</h1>
+        <h3>Mary-K Make up collection</h3>
+        <h1>Make up Enhances  <br></br> Your Natural Beauty</h1>
        <button>Shop Now</button>
       
        
